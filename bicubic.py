f51508586db3eac1445bf0d500f68866c943bc6c
Connor Stone <connorstone628@gmail.com>
2024-04-09 22:01:16 -0400

    fill out docstrings

--- bicubic.py
+++ bicubic.py
@@ -1,5 +1,12 @@
 import torch
 
+# Bicubic interpolation coefficients
+# These are the coefficients for the bicubic interpolation kernel.
+# To quote numerical recipes:
+#     The formulas that obtain the c’s from the function and derivative values
+#     are just a complicated linear transformation, with coefficients which,
+#     having been determined once in the mists of numerical history, can be
+#     tabulated and forgotten
 BC = (
     (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
     (0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0),
@@ -21,17 +28,22 @@ BC = (
 
 
 def bicubic_kernels(Z, d1, d2):
+    """
+    This is just a quick script to compute the necessary derivatives using
+    finite differences. This is not the most accurate way to compute the
+    derivatives, but it is good enough for most purposes.
+    """
     dZ1 = torch.zeros_like(Z)
     dZ2 = torch.zeros_like(Z)
     dZ12 = torch.zeros_like(Z)
 
     # First derivatives on first axis
-    # df/dx = (f(x+1, y) - f(x-1, y)) / 2
+    # df/dx = (f(x+h, y) - f(x-h, y)) / 2h
     dZ1[1:-1] = (Z[:-2] - Z[2:]) / (2 * d1)
     dZ1[0] = (Z[0] - Z[1]) / d1
     dZ1[-1] = (Z[-2] - Z[-1]) / d1
     # First derivatives on second axis
-    # df/dy = (f(x,y+1) - f(x,y-1)) / 2
+    # df/dy = (f(x,y+h) - f(x,y-h)) / h
     dZ2[:, 1:-1] = (Z[:, :-2] - Z[:, 2:]) / (2 * d2)
     dZ2[:, 0] = (Z[:, 0] - Z[:, 1]) / d2
     dZ2[:, -1] = (Z[:, -2] - Z[:, -1]) / d2
@@ -54,39 +66,82 @@ def interp_bicubic(
     get_ddY=False,
 ):
     """
-    Compute bicubic interpolation of a 2D grid at arbitrary locations.
+    Compute bicubic interpolation of a 2D grid at arbitrary locations. This will
+    smoothly interpolate a grid of points, including smooth first derivatives
+    and smooth cross derivative (d^2Y/dxdy). For the derivatives, continuity is
+    enforced, though the transition may be sharp as higher order derivatives are
+    not considered.
+
+    The interpolation requires knowing the values of the first derivative in
+    each axis and the cross derivative. If these are not provided, they will be
+    estimated using central differences. For this function, the derivatives
+    should be provided in pixel units. The interpolation will be more accurate
+    if an analytic value is available for the derivatives.
+
+    See Numerical Recipes in C, Chapter 3 (specifically: "Higher Order for
+    Smoothness: Bicubic Interpolaiton") for more details.
 
     Parameters
     ----------
     x : torch.Tensor
         x-coordinates of the points to interpolate. Must be a 0D or 1D tensor.
-        It should be in corner pixel units, meaning that x,y = 0,0 is the
-        position of the [0,0] pixel in the grid.
+        It should be in (-1,1) fov units, meaning that -1 is the left edge of
+        the left pixel, and 1 is the right edge of the right pixel.
     y : torch.Tensor
         y-coordinates of the points to interpolate. Must be a 0D or 1D tensor.
-        It should be in corner pixel units, meaning that x,y = 0,0 is the
-        position of the [0,0] pixel in the grid.
+        It should be in (-1,1) fov units, meaning that -1 is the bottom edge of
+        the bottom pixel, and 1 is the top edge of the top pixel.
     Z : torch.Tensor
         2D grid of values to interpolate. The first axis corresponds to the
         y-axis and the second axis to the x-axis. The values in Z correspond to
-        pixel corner values, so Z[0,0] is the value at the bottom left corner of
-        the grid. The grid should be at least 2x2 which would be the 4 corners
-        of a single pixel.
+        pixel center values, so Z[0,0] is the value at the center of the bottom
+        left corner pixel of the grid. The grid should be at least 2x2 so the
+        bicubic interpolation can go between the values.
     dZ1 : torch.Tensor or None
         First derivative of Z along the x-axis. If None, it will be estimated
-        using central differences.
+        using central differences. Note that the derivative should be computed
+        in pixel units, meaning that the distance from one pixel to the next is
+        considered "1" in these units.
     dZ2 : torch.Tensor or None
         First derivative of Z along the y-axis. If None, it will be estimated
-        using central differences.
+        using central differences. Note that the derivative should be computed
+        in pixel units, meaning that the distance from one pixel to the next is
+        considered "1" in these units.
     dZ12 : torch.Tensor or None
         Second derivative of Z along both axes. If None, it will be estimated
-        using central differences.
+        using central differences. Note that the derivative should be computed
+        in pixel units, meaning that the distance from one pixel to the next is
+        considered "1" in these units.
     get_Y : bool
-        Whether to return the interpolated values. This will add the estimated Y values to the return tuple
+        Whether to return the interpolated values. This will add the estimated Y
+        values to the return tuple
     get_dY : bool
-        Whether to return the interpolated first derivatives. This will add dY1 and dY2 to the return tuple
+        Whether to return the interpolated first derivatives. This will add dY1
+        and dY2 to the return tuple
     get_ddY : bool
-        Whether to return the interpolated second derivatives. This will add dY12, dY11, and dY22 to the return tuple
+        Whether to return the interpolated second derivatives. This will add
+        dY12, dY11, and dY22 to the return tuple
+
+    Returns
+    -------
+    Y : torch.Tensor or None
+        Interpolated values at the given locations. Only returned if get_Y is
+        True
+    dY1 : torch.Tensor or None
+        Interpolated first derivative along the x-axis. Only returned if get_dY
+        is True
+    dY2 : torch.Tensor or None
+        Interpolated first derivative along the y-axis. Only returned if get_dY
+        is True
+    dY12 : torch.Tensor or None
+        Interpolated second derivative along both axes. Only returned if get_ddY
+        is True
+    dY11 : torch.Tensor or None
+        Interpolated second derivative along the x-axis. Only returned if
+        get_ddY is True
+    dY22 : torch.Tensor or None
+        Interpolated second derivative along the y-axis. Only returned if
+        get_ddY is True
     """
 
     if Z.ndim != 2:
@@ -102,12 +157,10 @@ def interp_bicubic(
     x = x.clamp(-0.5, w - 0.5)
     y = 0.5 * ((y + 1) * h - 1)
     y = y.clamp(-0.5, h - 0.5)
-    d1 = 2.0 / w
-    d2 = 2.0 / h
 
     # Compute bicubic kernels if not provided
     if dZ1 is None or dZ2 is None or dZ12 is None:
-        _dZ1, _dZ2, _dZ12 = bicubic_kernels(Z, d1, d2)
+        _dZ1, _dZ2, _dZ12 = bicubic_kernels(Z, 1.0, 1.0)
     if dZ1 is None:
         dZ1 = _dZ1
     if dZ2 is None:
@@ -131,18 +184,18 @@ def interp_bicubic(
     v[:, 1] = Z[y0, x1]
     v[:, 2] = Z[y1, x1]
     v[:, 3] = Z[y1, x0]
-    v[:, 4] = dZ1[y0, x0] * d1
-    v[:, 5] = dZ1[y0, x1] * d1
-    v[:, 6] = dZ1[y1, x1] * d1
-    v[:, 7] = dZ1[y1, x0] * d1
-    v[:, 8] = dZ2[y0, x0] * d2
-    v[:, 9] = dZ2[y0, x1] * d2
-    v[:, 10] = dZ2[y1, x1] * d2
-    v[:, 11] = dZ2[y1, x0] * d2
-    v[:, 12] = dZ12[y0, x0] * d1 * d2
-    v[:, 13] = dZ12[y0, x1] * d1 * d2
-    v[:, 14] = dZ12[y1, x1] * d1 * d2
-    v[:, 15] = dZ12[y1, x0] * d1 * d2
+    v[:, 4] = dZ1[y0, x0]
+    v[:, 5] = dZ1[y0, x1]
+    v[:, 6] = dZ1[y1, x1]
+    v[:, 7] = dZ1[y1, x0]
+    v[:, 8] = dZ2[y0, x0]
+    v[:, 9] = dZ2[y0, x1]
+    v[:, 10] = dZ2[y1, x1]
+    v[:, 11] = dZ2[y1, x0]
+    v[:, 12] = dZ12[y0, x0]
+    v[:, 13] = dZ12[y0, x1]
+    v[:, 14] = dZ12[y1, x1]
+    v[:, 15] = dZ12[y1, x0]
 
     # Compute interpolation coefficients
     c = (torch.tensor(BC, dtype=v.dtype, device=v.device) @ v.unsqueeze(-1)).reshape(-1, 4, 4)
@@ -192,7 +245,7 @@ if __name__ == "__main__":
 
     # Create a 2D grid of random values
     np.random.seed(0)
-    Z = np.random.randn(10, 10)
+    Z = np.random.randn(10, 20)
 
     # Interpolate at random locations
     x = np.linspace(-1, 1, 101)
@@ -222,7 +275,7 @@ if __name__ == "__main__":
     print("Y")
     plt.imshow(Y, extent=(-1, 1, -1, 1), origin="lower", cmap="viridis")
     plt.colorbar()
-    plt.savefig("Y_est.png", dpi=200)
+    plt.savefig("Y_est.png")
     plt.close()
     print("dY1")
     plt.imshow(dY1, extent=(-1, 1, -1, 1), origin="lower", cmap="viridis")
